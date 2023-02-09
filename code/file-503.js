cfec184a2ff65ac51f2199f3a6aef3b4bc185b20
GooGee <mou-gj@live.com>
2023-02-10 07:01:54 +0800

    build002

--- code/file-503.js
+++ code/file-503.js
@@ -3,25 +3,47 @@ function run(data) {
     const ddd = data
 
     /** @type {Map<number, LB.Column[]>} */
-    const columnmap = new Map()
+    const eiczzm = new Map()
     ddd.db.tables.Column.forEach((item) => {
-        if (item.inTable === false) {
-            return
-        }
-        let found = columnmap.get(item.entityId)
+        // if (item.inTable === false) {
+        //     return
+        // }
+        let found = eiczzm.get(item.entityId)
         if (found === undefined) {
             found = []
-            columnmap.set(item.entityId, found)
+            eiczzm.set(item.entityId, found)
+        }
+        found.push(item)
+    })
+
+    /** @type {Map<number, LB.Wu[]>} */
+    const eiwzzm = new Map()
+    ddd.db.tables.Wu.forEach((item) => {
+        let found = eiwzzm.get(item.entityId)
+        if (found === undefined) {
+            found = []
+            eiwzzm.set(item.entityId, found)
+        }
+        found.push(item)
+    })
+
+    /** @type {Map<number, LB.WuParameter[]>} */
+    const wiwpzzm = new Map()
+    ddd.db.tables.WuParameter.forEach((item) => {
+        let found = wiwpzzm.get(item.wuId)
+        if (found === undefined) {
+            found = []
+            wiwpzzm.set(item.wuId, found)
         }
         found.push(item)
     })
 
     ddd.typezz = ddd.db.tables.Entity.map((item) => {
         const linezz = [`    interface ${item.name} {`]
-        const columnzz = columnmap.get(item.id)
+        const columnzz = eiczzm.get(item.id)
         if (columnzz) {
             columnzz.forEach((item) => {
-                linezz.push(`        ${item.name}: ${getType(item)}`)
+                linezz.push(`        ${item.name}: ${makeType(item)}`)
             })
         }
         linezz.push("    }")
@@ -34,9 +56,25 @@ function run(data) {
      * @returns {string}
      */
     function getType(item) {
-        if (item.dtoType === "integer") {
+        if (item.tf.type === "integer") {
             return "number"
         }
-        return item.dtoType
+        return item.tf.type
+    }
+
+    /**
+     *
+     * @param {LB.Column} item
+     * @returns {string}
+     */
+    function makeType(item) {
+        let zz = getType(item)
+        if (item.tf.isArray) {
+            zz += '[]'
+        }
+        if (item.tf.nullable) {
+            zz += ' | null'
+        }
+        return zz
     }
 }
